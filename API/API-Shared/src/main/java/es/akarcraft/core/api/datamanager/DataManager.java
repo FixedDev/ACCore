c31a88080eeb0a9024ee9c98cffaa07972cc2240
Gilberto Garcia <gilbertodamian14@gmail.com>
2019-12-21 19:37:28 -0600

    Create the basic interface for the DataManager

---
+++ API/API-Shared/src/main/java/es/akarcraft/core/api/datamanager/DataManager.java
@@ -0,0 +1,19 @@
+package es.akarcraft.core.api.datamanager;
+
+import com.google.common.util.concurrent.ListenableFuture;
+
+import java.util.List;
+
+public interface DataManager<K, O> {
+    O getObject(K key);
+
+    ListenableFuture<O> getObjectAsync(K key);
+
+    List<O> getObjects(List<K> keys);
+
+    ListenableFuture<O> getObjectsAsync(List<K> keys);
+
+    ListenableFuture<O> deleteObject(K key);
+
+    ListenableFuture<?> save(K key, O object);
+}
