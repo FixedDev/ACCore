e7af604c434e828a5101a49cf96914551cd29028
Gilberto Garcia <gilbertodamian14@gmail.com>
2019-12-21 20:00:04 -0600

    Add the getOrFind method, since the getObject method isn't cached, so we need a method that mimics the behaviour of getObject and also uses the cache

--- API/API-Shared/src/main/java/es/akarcraft/core/api/datamanager/CachedDataManager.java
+++ API/API-Shared/src/main/java/es/akarcraft/core/api/datamanager/CachedDataManager.java
@@ -1,9 +1,13 @@
 package es.akarcraft.core.api.datamanager;
 
+import com.google.common.util.concurrent.ListenableFuture;
+
 import java.util.Optional;
 
 public interface CachedDataManager<K, O> extends DataManager<K, O> {
     Optional<O> getIfCached(K key);
+
+    ListenableFuture<O> getOrFind(K key);
 
     void cache(O object);
 
