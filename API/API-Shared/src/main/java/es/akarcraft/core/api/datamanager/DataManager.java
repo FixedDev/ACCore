2206fedbb604542fa3ea36ea745d0479e467fd8e
Gilberto Garcia <gilbertodamian14@gmail.com>
2019-12-21 20:25:33 -0600

    Add documentation to the methods

--- API/API-Shared/src/main/java/es/akarcraft/core/api/datamanager/DataManager.java
+++ API/API-Shared/src/main/java/es/akarcraft/core/api/datamanager/DataManager.java
@@ -3,15 +3,48 @@ package es.akarcraft.core.api.datamanager;
 import com.google.common.util.concurrent.ListenableFuture;
 
 import java.util.List;
+import java.util.Optional;
 
+/**
+ * This class manages the serialization of the object of type {@param O}
+ * and also provides the basic operations like get, delete and save(update) for this type of object
+ * on a datastore
+ *
+ * @param <K> The key of the object
+ * @param <O> The actual object type
+ */
 public interface DataManager<K, O> {
-    O getObject(K key);
+    /**
+     * Finds an object on the datastore with the specified key
+     * @param key The key of the object to find
+     *
+     * @return An optional object of type {@param O}, absent if it doesn't exists on the datastore
+     */
+    Optional<O> getObject(K key);
 
+    /**
+     * Finds an object using another thread on the datastore with the specified key
+     * @param key The key of the object to find
+     *
+     * @return A ListenableFuture of type {@param O}, the object inside the Future can be null,
+     *          that means that the object doesn't exists on the datastore
+     */
     ListenableFuture<O> getObjectAsync(K key);
 
+    /**
+     * Finds a list of objects using a provided list of {@param keys} on the datastore
+     * @param keys The keys to search on the datastore
+     * @return The list of the objects found in the same order as the keys
+     */
     List<O> getObjects(List<K> keys);
 
-    ListenableFuture<O> getObjectsAsync(List<K> keys);
+    /**
+     * The principal behaviour of this method is the same as {@see getObjects(List<K>)}
+     * but it's executed on another thread
+     * @param keys The keys to find
+     * @return A future object that contains the list of the objects found in the same order as the keys
+     */
+    ListenableFuture<List<O>> getObjectsAsync(List<K> keys);
 
     ListenableFuture<O> deleteObject(K key);
 
